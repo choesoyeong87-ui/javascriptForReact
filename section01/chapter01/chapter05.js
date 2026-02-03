0ecf52bd26f27fe5da7bc51baab39bcbca480ff8
chesonsn12 <chesonsnlscghrc@naver.com>
2026-02-03 11:12:54 +0900

    [추가]자바스크립트 데이터타입

---
+++ section01/chapter01/chapter05.js
@@ -0,0 +1,18 @@
+// 1. Number Type 
+// let inf = Infinity;
+// let mInf = -Infinity;
+// let nan = NaN; //not a number
+// console.log(inf);
+// console.log(mInf);
+// console.log(nan);
+// console.log(typeof inf);
+// console.log(typeof nan);
+
+//템플릿 리터널
+let name = '최소영';
+let age = 20;
+let introduceText = name + "님은 나이가"+
+age + "입니다";
+let introduceText2 =` ${name} + "님은 나이가"+
+${age} + "입니다"`;
+console.log(introduceText);
