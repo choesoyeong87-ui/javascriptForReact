49582362123ca2496eadf8e389670bd4369ac65b
chesonsn12 <chesonsnlscghrc@naver.com>
2026-02-09 11:48:25 +0900

    [생성] 사용자 정의하기 useState()

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import HookExam from './components/HookExam'
 import Register from './components/Register'
+
 
 
 export default function App() {
  
   return<>
-  <Register/>
+  <HookExam/>
   </>
 
 }
