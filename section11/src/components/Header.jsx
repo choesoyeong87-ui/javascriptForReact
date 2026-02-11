95016b9dfc5b5ded3825ba9c00cec555f0575457
chesonsn12 <chesonsnlscghrc@naver.com>
2026-02-11 15:24:49 +0900

    [성능개선] useMemo useCallback 기능추가

--- section11/src/components/Header.jsx
+++ section11/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import '../css/Header.css'
+import { memo } from 'react';
 
-const Header =()=>{
-
+const Header =({count})=>{
+console.log(`HEADER ${count}`)
     return<>
     <div className="Header">
     <h3>오늘은 달력📆</h3> 
@@ -11,4 +12,5 @@ const Header =()=>{
     </>
 
 }
-export default Header;
+const memonizedHeader = memo(Header);
+export default memonizedHeader;
